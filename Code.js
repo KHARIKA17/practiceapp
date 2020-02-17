01544fb7a062f8b67c437017b3bf7cad9730743b
KHARIKA17 <60010885+KHARIKA17@users.noreply.github.com>
2020-02-17 13:06:41 -0600

    ASSG

---
+++ Code.js
@@ -0,0 +1,14 @@
+function calculate() {
+    let totalIn = parseFloat(document.getElementById("totalIN").value);
+    console.log(typeof totalIn);
+    let rateIn = parseFloat(document.getElementById("rateIN").value);
+    if (totalIn == "" || rateIn == "") {
+        alert("Enter the ammount");
+    }
+    document.getElementById("Tip").innerHTML = "$" + (totalIn * rateIn)/100;
+    document.getElementById("Tax").innerHTML = "$" + 5.5;
+    document.getElementById("GrandTotal").innerHTML = "$" + (totalIn + ((totalIn * rateIn)/100) + 5.5);
+    return false;
+
+    
+}
